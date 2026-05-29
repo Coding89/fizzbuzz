1dc06a0fe5a0e79d1ecff6303a29d6b5d59c3911
Coding89 <timothy.lefevre1@gmail.com>
2026-05-29 18:03:48 +0100

    add .gitignore.txt

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,10 @@
 def fizz_buzz(x):
     for i in range(1, x+1):
-        if i % 5 == 0 and i % 10 == 0:
+        if i % 3 == 0 and i % 5 == 0:
             print("Fizzbuzz")
-        elif i % 5 == 0:
+        elif i % 3 == 0:
             print("Fizz")
-        elif i % 10 == 0:
+        elif i % 5 == 0:
             print("Buzz")
         else: 
             print(i)
